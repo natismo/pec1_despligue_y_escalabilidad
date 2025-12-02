5ac04795c32073fc030e21b7d650ec54222bcbc8
natismo <natisepulvedamora@gmail.com>
2025-12-01 21:29:47 -0300

    creación función conteo

---
+++ count_numbers.py
@@ -0,0 +1,8 @@
+import time
+
+print("--- Iniciando conteo de números ---")
+for i in range(1, 11):
+    print(f"Número: {i}")
+    time.sleep(1) # Pausa de 1 seg para ver el proceso en tiempo real
+print("--- Fin del conteo ---")
+
